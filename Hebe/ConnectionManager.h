8f97cb790b69bda4d07dad4de3c2b519da4cd6fc
Martin Candela <rellikiox@gmail.com>
2012-08-12 04:29:52 +0200

    I start to understand boost

--- Hebe/ConnectionManager.h
+++ Hebe/ConnectionManager.h
@@ -4,8 +4,10 @@
 #define NULL 0
 #endif
 
-#include <boost\asio.hpp>
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -28,6 +30,23 @@ namespace hebe {
 	private:
 		static ConnectionManager * _instance;
 		ConnectionManager(void);
+
+		struct Packet{
+			Packet(int o, int i, string m) : op_code(o), id(i), message(m.c_str()) {}
+			boost::array<boost::asio::mutable_buffer, 3> getPacket() {
+				boost::array<int, 1> o = { op_code };
+				boost::array<int, 1> i = { id };
+				//boost::array<char, 512> m = message;
+				boost::array<boost::asio::mutable_buffer, 3> p = {
+					boost::asio::buffer(o),
+					boost::asio::buffer(i),
+					boost::asio::buffer(message)};
+			}
+
+			int op_code;
+			int id;
+			boost::array<char, 256> message;
+		};
 	};
 
 }
